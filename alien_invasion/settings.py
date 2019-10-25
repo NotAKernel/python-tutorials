1ade647117e5f3216d1a421f3d3c4dafc3f7f22d
Loaner <daniely101202@gmail.com>
2019-10-25 15:58:18 +0100

    starting to add ship

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -5,5 +5,5 @@ class Settings:
         """Initialise the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.scren_height = 800
+        self.screen_height = 800
         self.bg_colour = (230, 230, 230)
